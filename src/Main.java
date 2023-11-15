850b8479bc1c8a8a0d191c676fa8cecf2342f2dd
MiloszDEV <miloszz.krakowiak@gmail.com>
2023-11-15 09:35:58 +0100

    Zadanie zrobione

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Commit testowy");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj ciąg znaków");
+        String ciag = scanner.nextLine();
+        if(ciag.length()>=3){
+            String ciag2 = ciag.substring(ciag.length()-2,ciag.length());
+            ciag2 = ciag2.concat(ciag.substring(0,ciag.length()-2));
+            System.out.println(ciag2);
+        }
+        else{
+            System.out.println("Za krótki ciąg!");
+        }
 
     }
 }
